0f0c2d17503545dd0a0eb2be67895a7294995d57
Joel Weiser <joel.weiser@oicr.on.ca>
2020-01-14 14:45:48 -0500

    removes commented out lines

--- src/test/java/org/reactome/release/update_stable_ids/StableIdentifierUpdaterTest.java
+++ src/test/java/org/reactome/release/update_stable_ids/StableIdentifierUpdaterTest.java
@@ -20,93 +20,91 @@ import java.util.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({StableIdentifierUpdater.class, InstanceEditUtils.class})
-@PowerMockIgnore({"org.apache.logging.log4j.*", "javax.management.*", "javax.script.*",
-        "javax.xml.*", "com.sun.org.apache.xerces.*", "org.xml.sax.*", "com.sun.xml.*", "org.w3c.dom.*", "org.mockito.*"})
+@PowerMockIgnore({
+	"org.apache.logging.log4j.*", "javax.management.*", "javax.script.*", "javax.xml.*", "com.sun.org.apache.xerces.*",
+	"org.xml.sax.*", "com.sun.xml.*", "org.w3c.dom.*", "org.mockito.*"
+})
 public class StableIdentifierUpdaterTest {
 
-    private MySQLAdaptor mockSliceAdaptor = PowerMockito.mock(MySQLAdaptor.class);
-    private MySQLAdaptor mockPrevSliceAdaptor = PowerMockito.mock(MySQLAdaptor.class);
-    private MySQLAdaptor mockGkCentralAdaptor = PowerMockito.mock(MySQLAdaptor.class);
+	private MySQLAdaptor mockSliceAdaptor = PowerMockito.mock(MySQLAdaptor.class);
+	private MySQLAdaptor mockPrevSliceAdaptor = PowerMockito.mock(MySQLAdaptor.class);
+	private MySQLAdaptor mockGkCentralAdaptor = PowerMockito.mock(MySQLAdaptor.class);
 
-//    @Mock
-//    UpdateStableIds stableUpdater = new UpdateStableIds();
+	@Mock
+	private GKInstance mockInstanceEdit;
 
-    @Mock
-    private GKInstance mockInstanceEdit;
+	@Mock
+	private GKInstance mockInstance;
+	@Mock
+	private GKInstance mockInstance2;
+	@Mock
+	private GKInstance mockInstance3;
+	@Mock
+	private GKInstance mockInstanceNull = null;
 
-    @Mock
-    private GKInstance mockInstance;
-    @Mock
-    private GKInstance mockInstance2;
-    @Mock
-    private GKInstance mockInstance3;
-    @Mock
-    private GKInstance mockInstanceNull = null;
+	@Mock
+	private SchemaClass mockSchemaClass;
 
-    @Mock
-    private SchemaClass mockSchemaClass;
-
-    private List<GKInstance> sliceList;
-    private List<GKInstance> sliceList2;
-    private List<GKInstance> sliceListNull;
+	private List<GKInstance> sliceList;
+	private List<GKInstance> sliceList2;
+	private List<GKInstance> sliceListNull;
 
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-    @Test
-    public void updateStableIdentifiersTest() throws Exception {
+	@Test
+	public void updateStableIdentifiersTest() throws Exception {
 
-        PowerMockito.mockStatic(InstanceEditUtils.class);
-//        Mockito.when(InstanceEditUtils.createInstanceEdit(mockAdaptor, 12345L, "CreatorName")).thenReturn(mockInstanceEdit);
+		PowerMockito.mockStatic(InstanceEditUtils.class);
 
-        sliceList = Arrays.asList(mockInstance, mockInstance, mockInstance);
-        sliceList2 = Arrays.asList(mockInstance, mockInstance);
+		sliceList = Arrays.asList(mockInstance, mockInstance, mockInstance);
+		sliceList2 = Arrays.asList(mockInstance, mockInstance);
 
-        Mockito.when(mockSliceAdaptor.fetchInstancesByClass("Event")).thenReturn(sliceList);
-        Mockito.when(mockSliceAdaptor.fetchInstancesByClass("PhysicalEntity")).thenReturn(sliceList);
+		Mockito.when(mockSliceAdaptor.fetchInstancesByClass("Event")).thenReturn(sliceList);
+		Mockito.when(mockSliceAdaptor.fetchInstancesByClass("PhysicalEntity")).thenReturn(sliceList);
 
-        Mockito.when(mockPrevSliceAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance2);
-        Mockito.when(mockGkCentralAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance3);
+		Mockito.when(mockPrevSliceAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance2);
+		Mockito.when(mockGkCentralAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance3);
 
-        Mockito.when(mockInstance.getAttributeValuesList("modified")).thenReturn(sliceList);
-        Mockito.when(mockInstance2.getAttributeValuesList("modified")).thenReturn(sliceList2);
+		Mockito.when(mockInstance.getAttributeValuesList("modified")).thenReturn(sliceList);
+		Mockito.when(mockInstance2.getAttributeValuesList("modified")).thenReturn(sliceList2);
 
-        Mockito.when(mockInstance.getAttributeValue("stableIdentifier")).thenReturn(mockInstance);
-        Mockito.when(mockInstance3.getAttributeValue("stableIdentifier")).thenReturn(mockInstance);
+		Mockito.when(mockInstance.getAttributeValue("stableIdentifier")).thenReturn(mockInstance);
+		Mockito.when(mockInstance3.getAttributeValue("stableIdentifier")).thenReturn(mockInstance);
 
-        Mockito.when(mockInstance.getAttributeValue("identifierVersion")).thenReturn("1");
+		Mockito.when(mockInstance.getAttributeValue("identifierVersion")).thenReturn("1");
 
-        Mockito.when(mockInstance.getSchemClass()).thenReturn(mockSchemaClass);
-        Mockito.when(mockInstance.getSchemClass().isa("Event")).thenReturn(true);
+		Mockito.when(mockInstance.getSchemClass()).thenReturn(mockSchemaClass);
+		Mockito.when(mockInstance.getSchemClass().isa("Event")).thenReturn(true);
 
-        Mockito.when(mockInstance.getAttributeValuesList("reviewed")).thenReturn(sliceList);
-        Mockito.when(mockInstance2.getAttributeValuesList("reviewed")).thenReturn(sliceList2);
+		Mockito.when(mockInstance.getAttributeValuesList("reviewed")).thenReturn(sliceList);
+		Mockito.when(mockInstance2.getAttributeValuesList("reviewed")).thenReturn(sliceList2);
 
-        StableIdentifierUpdater.updateStableIdentifiers(mockSliceAdaptor, mockPrevSliceAdaptor, mockGkCentralAdaptor, 12345L);
-    }
+		StableIdentifierUpdater.updateStableIdentifiers(mockSliceAdaptor, mockPrevSliceAdaptor, mockGkCentralAdaptor, 12345L);
+	}
 
-    @Test
-    public void updateStableIdentifiersModifiedListsWithSameSizeTest() throws Exception {
-        PowerMockito.mockStatic(InstanceEditUtils.class);
+	@Test
+	public void updateStableIdentifiersModifiedListsWithSameSizeTest() throws Exception {
+		PowerMockito.mockStatic(InstanceEditUtils.class);
 
-        sliceList = Arrays.asList(mockInstance);
-        sliceList2 = Arrays.asList(mockInstance, mockInstance);
+		sliceList = Arrays.asList(mockInstance);
+		sliceList2 = Arrays.asList(mockInstance, mockInstance);
 
-        Mockito.when(mockSliceAdaptor.fetchInstancesByClass("Event")).thenReturn(sliceList);
-        Mockito.when(mockSliceAdaptor.fetchInstancesByClass("PhysicalEntity")).thenReturn(sliceList);
+		Mockito.when(mockSliceAdaptor.fetchInstancesByClass("Event")).thenReturn(sliceList);
+		Mockito.when(mockSliceAdaptor.fetchInstancesByClass("PhysicalEntity")).thenReturn(sliceList);
 
-        Mockito.when(mockPrevSliceAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance2);
-        Mockito.when(mockGkCentralAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance3);
+		Mockito.when(mockPrevSliceAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance2);
+		Mockito.when(mockGkCentralAdaptor.fetchInstance(mockInstance.getDBID())).thenReturn(mockInstance3);
 
-        Mockito.when(mockInstance.getAttributeValuesList("modified")).thenReturn(sliceList);
-        Mockito.when(mockInstance2.getAttributeValuesList("modified")).thenReturn(sliceList2);
+		Mockito.when(mockInstance.getAttributeValuesList("modified")).thenReturn(sliceList);
+		Mockito.when(mockInstance2.getAttributeValuesList("modified")).thenReturn(sliceList2);
 
-        Mockito.when(mockInstance.getSchemClass()).thenReturn(mockSchemaClass);
-        Mockito.when(mockInstance.getSchemClass().isa("Event")).thenReturn(false);
+		Mockito.when(mockInstance.getSchemClass()).thenReturn(mockSchemaClass);
+		Mockito.when(mockInstance.getSchemClass().isa("Event")).thenReturn(false);
 
-        StableIdentifierUpdater.updateStableIdentifiers(mockSliceAdaptor, mockPrevSliceAdaptor, mockGkCentralAdaptor, 12345L);
-    }
+		StableIdentifierUpdater.updateStableIdentifiers(mockSliceAdaptor, mockPrevSliceAdaptor, mockGkCentralAdaptor, 12345L);
+	}
 }
